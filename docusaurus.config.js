a37e9ad26ea98995c6ab418f8102f011b8f69f35
MrUniverse44 <oficial.mruniverse@gmail.com>
2024-09-21 17:35:00 -0500

    fix: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,13 +16,14 @@ const config = {
   url: 'https://mruniverse44.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mruniverse44.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MrUniverse44', // Usually your GitHub org/user name.
+  deploymentBranch: 'gh-pages',
   projectName: 'mruniverse44.github.io', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
